e2a7269873973d9f10d99271ca2a401bcc98c355
vishnuskk24 <vishuskk24@gmail.com>
2025-01-07 23:08:09 +0530

    Update DemoApplication.java
    Added some code to allow the request from the swagger

--- InfyMeMobile/src/main/java/com/infy/DemoApplication.java
+++ InfyMeMobile/src/main/java/com/infy/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -38,6 +39,17 @@ class WebConfig implements WebMvcConfigurer {
     @Bean
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("otpCache");
+    }
+    
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://imobile-production.up.railway.app")  // Replace with the Swagger UI URL
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*");
+        }
     }
 }
 // creepyguyfromsrilanka$1
